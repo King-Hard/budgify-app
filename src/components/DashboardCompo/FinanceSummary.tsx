0fda4d273596cd268c1b9d7fd7dc907888063034
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-08-10 21:00:57 +0800

    .

--- src/components/DashboardCompo/FinanceSummary.tsx
+++ src/components/DashboardCompo/FinanceSummary.tsx
@@ -58,7 +58,7 @@ export default async function FinanceSummary() {
       </div>
 
       <div>
-        <h1></h1>
+        <h1>HAHHAHAHAHA</h1>
       </div>
 
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
