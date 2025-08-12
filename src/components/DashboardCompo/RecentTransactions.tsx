c09e7bdc998b4fb602c6e39c07c44a7f240abd46
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-08-12 19:07:50 +0800

    .

--- src/components/DashboardCompo/RecentTransactions.tsx
+++ src/components/DashboardCompo/RecentTransactions.tsx
@@ -50,6 +50,7 @@ export default async function RecentTransactions() {
 
       <div>
         <h1>HAHAHAHHA</h1>
+        <p></p>
       </div>
 
         <div className="flex justify-between items-center">
