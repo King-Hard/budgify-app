882c137d746e6e9ee405b28776e32b5a48fced2d
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-09-01 18:22:18 +0800

    .

--- src/app/budgify/bill/page.tsx
+++ src/app/budgify/bill/page.tsx
@@ -157,7 +157,9 @@ export default function BillReminder() {
               HAHAHAHA
             </p>
             <div>
-              
+              <h1>
+                
+              </h1>
             </div>
           </div>
 
